851848be84e83c732922aded6a6cbce496af2706
whatakel <kelvinmlcardoso@gmail.com>
2025-06-09 13:22:22 -0300

    adicionar tarefa

--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c90bb271b41ade51d1d1890795f0541630bbe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061b6878765f291a3ec7ee530fe5317b71561e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
